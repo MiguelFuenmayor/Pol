d193d97230167735f4c0117820f6653cdc410175
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2024-01-22 14:15:50 -0400

    characteristics table added

---
+++ database/migrations/2024_01_22_123707_create_characteristics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characteristics', function (Blueprint $table) {
+            $table->foreignId('functionary_id')->constrained();
+            $table->string('height_in_cm',20);
+            $table->string('hair_color',20);
+            $table->string('skin_color',20);
+            $table->string('weight_in_kg');
+            $table->string('other_characteristics',70);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characteristics');
+    }
+};
