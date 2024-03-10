aa04f7f90982947fc5a7526c7405f2584b057873
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2024-03-10 09:32:00 -0400

    changing to spatie/laravel-hatml

--- resources/views/livewire/functionary-create.blade.php
+++ resources/views/livewire/functionary-create.blade.php
@@ -4,17 +4,8 @@
         FORMULARIO FUNCIONARIO
     @endslot
     <div class="p-2 m-2 bg-white rounded shadow-lg">
-        <!--usar laravel collective, estudiar como funciona-->
-        {!! Form::open([ 'files'=>true])!!}
-            {!! Form::text('names') !!}
-            {!! Form::text('surnames') !!}
-            {!! Form::select('dependency', ['1','11','12'], '3') !!}
-            {!! Form::select('rank', ['1','2','3'], '1') !!}
-            {!! Form::select('gender', [1=>'masculino',2=>'femenino'], '1') !!}
-            
-            
-            
-        {!! Form::close() !!}
+        <!--Laravel collective html is abandoned, so I shift to Spatie Laravael-html-->
+        
     </div>
 
 </div>
