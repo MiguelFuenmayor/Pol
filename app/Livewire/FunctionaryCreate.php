dddb972818ebba0a39f0ee19e1ed143489238bbf
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2024-03-11 19:25:53 -0400

    making form with laravel html

--- app/Livewire/FunctionaryCreate.php
+++ app/Livewire/FunctionaryCreate.php
@@ -1,15 +1,68 @@
 <?php
 
 namespace App\Livewire;
+
 use App\Models\Functionary;
+use App\Models\Promo;
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use App\Models\Rank;
+use App\Models\Charge;
+use App\Models\Dependency;
+use App\Models\Status;
+
 #[Title('Añadiendo funcionario')]
 class FunctionaryCreate extends Component
 {
+    public $names;
+    public $functionary;
+    public $data;
+    public function mount()
+    {
+        
+        $this->functionary = [
+            'surnames' => null,
+            'names' => null,
+            'age' => null,
+            'gender_id' => null,
+            'identity_document' => null,
+            'credential' => null,
+            'start_date' => null,
+            'end_date' => null,
+            'user_id"' => null,
+            'promo_id' => null,
+            'status_id' => null,
+            'rank_id' => null,
+            'dependency_id' => null,
+            'created_at' => null,
+            'updated_at' => null
+        ];
+
+        $this->data = [
+            'promos' => $this->SelectFormat(Promo::all()->toArray(), 'promo'),
+            'ranks' => $this->SelectFormat(Rank::all()->toArray()),
+            'charges' => $this->SelectFormat(Charge::all()->toArray(), 'charge'),
+            'dependencies' => $this->SelectFormat(Dependency::all()->toArray()),
+            'statuses' => $this->SelectFormat(Status::all()->toArray())
+        ];
+    }
+
+    public function SelectFormat(array $data, $column = 'name')
+    {
+        $result = [];
+        $i = 0;
+        while ($i < count($data)) {
+            $result[] = $data[$i][$column];
+            $i++;
+        }
+
+
+        return $result;
+    }
+
     public function render()
     {
-        return view('livewire.functionary-create');
+        return view('livewire.functionary-create', ['data' => $this->data, 'names' => $this->names]);
     }
 
     public function save()
--- resources/views/livewire/functionary-create.blade.php
+++ resources/views/livewire/functionary-create.blade.php
@@ -1,16 +1,36 @@
 <div>
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
     @slot('header')
-        FORMULARIO FUNCIONARIO
+        RELLENE EL FORMULARIO PARA AÑADIR FUNCIONARIO
     @endslot
-    <div class="p-2 m-2 bg-white rounded shadow-lg">
+    <div class="p-2 m-2 bg-white rounded shadow-md">
         <!--Laravel collective html is abandoned, so I shift to Spatie Laravael-html-->
         
-        {{ html()->form('PUT')->open() }}
-
-            {{ html()->text('names') }}
-            {{ html()->text('surnames') }}
-        {{ html()->form()->close() }}
+        <form class="">
+            {{ html()->token()}}
+            {{ html()->text('names')->attributes(['placeholder'=>'Ingrese los nombres','wire:model'=>'names'])->class('row-start-1') }} <br/>
+            {{ html()->text('surnames')->attribute('placeholder','Ingrese los apellidos')->class('col-start-2') }}<br/>
+            {{ html()->text('age')->attributes(['pattern'=>'[0-9]{2}','placeholder'=>'Ingrese la edad']) }} <br/>
+            {{ html()->label('Seleccione el genero','gender_id') }} <br/>
+            {{ html()->select('gender_id',[1=>'Masculino',2=>'Femenino'])->class('pr-10 p-2 m2')}} <br/>
+            {{ html()->text('credential')->attributes(['placeholder'=>'Ingrese la credencial','pattern'=> '[0-9]{9}'])}} <br/>
+            {{ html()->text('identity_document')->attributes(['placeholder'=>'Ingrese la cedula'])}} <br/>
+            {{ html()->label('Seleccione la promo','promo_id') }} <br/>
+            {{ html()->select('promo_id',$data['promos']) }} <br/>
+            {{ html()->label('Seleccione el rango','rank_id') }} <br/>
+            {{ html()->select('rank_id',$data['ranks']) }} <br/>
+            {{ html()->label('Seleccione el cargo','charge_id') }} <br/>
+            {{ html()->select('charge_id',$data['charges'])->class('pr-10') }} <br/>
+            {{ html()->label('Seleccione la dependencia','dependency_id') }} <br/>
+            {{ html()->select('dependency_id',$data['dependencies'])->class('pr-10') }} <br/>
+            {{ html()->label('Seleccione el estado','status_id') }} <br/>
+            {{ html()->select('status_id',$data['statuses'])->class('pr-10') }} <br/>
+            {{ html()->label('Seleccione fecha de ingreso','start_date') }} <br/>
+            {{ html()->input('date','start_date')}}<br/>
+            {{ html()->label('Seleccione fecha de egreso','end_date') }} <br/>
+            {{ html()->input('date','end_date')}} <br/>
+            {{html()->submit('enviar')->class('p-2 m-2 bg-blue-600')}}
+        </form>
         
     </div>
 
