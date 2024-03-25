04ec8a7628ceb51ca33a2f8e79755b098df9c72c
Miguel Fuenmayor <miguelfuenmayormn@gmail.com>
2024-03-25 11:27:34 -0400

    styles to Functionary add form

--- app/Livewire/FunctionaryCreate.php
+++ app/Livewire/FunctionaryCreate.php
@@ -11,15 +11,51 @@ use App\Models\Charge;
 use App\Models\Dependency;
 use App\Models\Status;
 
+use Livewire\Attributes\Validate;
+
 #[Title('Añadiendo funcionario')]
 class FunctionaryCreate extends Component
-{
+{   #[Validate('required', message:'Debe ingresar un nombre')]
+    #[Validate('min:3', message: 'Ingrese un nombre con mas de 3 caracteres')]
     public $names;
+    #[Validate('required', message:'Debe ingresar un apellido')]
+    public $surnames;
+    #[Validate('required', message:'Debe ingresar una edad')]
+    public $age;
+    // #[Validate('required', message:'Seleccione un genero')]
+    public $gender_id;
+    #[Validate('required')]
+    public $identity_document;
+    #[Validate('required')]
+    public $credential;
+    #[Validate('required')]
+    public $start_date;
+    // #[Validate('required')]
+    // public $end_date;
+    #[Validate('required')]
+    public $charge_id;
+    
+    // #[Validate('required')]
+    public $promo_id;
+    // #[Validate('required')]
+    public $status_id;
+    // #[Validate('required')]
+    public $rank_id;
+    // #[Validate('required')]
+    public $dependency_id;
+    public $user_id;
     public $functionary;
     public $data;
+
     public function mount()
     {
-        
+        $this->gender_id=1;
+        $this->charge_id=0;
+        $this->promo_id=0;
+        $this->dependency_id=0;
+        $this->rank_id=0;
+        $this->promo_id=0;
+        $this->status_id=0;
         $this->functionary = [
             'surnames' => null,
             'names' => null,
@@ -28,14 +64,13 @@ class FunctionaryCreate extends Component
             'identity_document' => null,
             'credential' => null,
             'start_date' => null,
-            'end_date' => null,
-            'user_id"' => null,
+            // 'end_date' => null,
+            'user_id' => null,
             'promo_id' => null,
+            'charge_id'=> null,
             'status_id' => null,
             'rank_id' => null,
-            'dependency_id' => null,
-            'created_at' => null,
-            'updated_at' => null
+            'dependency_id' => null
         ];
 
         $this->data = [
@@ -66,7 +101,16 @@ class FunctionaryCreate extends Component
     }
 
     public function save()
-    {
-        return true;
+    {   
+        $this->validate();
+        foreach($this->functionary as $property => $value){
+            $this->functionary[$property]=$this->$property;
+        }
+        $this->functionary['charge_id']++;
+        $this->functionary['promo_id']++;
+        $this->functionary['dependency_id']++;
+        $this->functionary['rank_id']++;
+        $this->functionary['status_id']++;
+        return dd($this->functionary);
     }
 }
--- resources/views/livewire/functionary-create.blade.php
+++ resources/views/livewire/functionary-create.blade.php
@@ -6,30 +6,83 @@
     <div class="p-2 m-2 bg-white rounded shadow-md">
         <!--Laravel collective html is abandoned, so I shift to Spatie Laravael-html-->
         
-        <form class="">
-            {{ html()->token()}}
-            {{ html()->text('names')->attributes(['placeholder'=>'Ingrese los nombres','wire:model'=>'names'])->class('row-start-1') }} <br/>
-            {{ html()->text('surnames')->attribute('placeholder','Ingrese los apellidos')->class('col-start-2') }}<br/>
-            {{ html()->text('age')->attributes(['pattern'=>'[0-9]{2}','placeholder'=>'Ingrese la edad']) }} <br/>
-            {{ html()->label('Seleccione el genero','gender_id') }} <br/>
-            {{ html()->select('gender_id',[1=>'Masculino',2=>'Femenino'])->class('pr-10 p-2 m2')}} <br/>
-            {{ html()->text('credential')->attributes(['placeholder'=>'Ingrese la credencial','pattern'=> '[0-9]{9}'])}} <br/>
-            {{ html()->text('identity_document')->attributes(['placeholder'=>'Ingrese la cedula'])}} <br/>
-            {{ html()->label('Seleccione la promo','promo_id') }} <br/>
-            {{ html()->select('promo_id',$data['promos']) }} <br/>
-            {{ html()->label('Seleccione el rango','rank_id') }} <br/>
-            {{ html()->select('rank_id',$data['ranks']) }} <br/>
-            {{ html()->label('Seleccione el cargo','charge_id') }} <br/>
-            {{ html()->select('charge_id',$data['charges'])->class('pr-10') }} <br/>
-            {{ html()->label('Seleccione la dependencia','dependency_id') }} <br/>
-            {{ html()->select('dependency_id',$data['dependencies'])->class('pr-10') }} <br/>
-            {{ html()->label('Seleccione el estado','status_id') }} <br/>
-            {{ html()->select('status_id',$data['statuses'])->class('pr-10') }} <br/>
-            {{ html()->label('Seleccione fecha de ingreso','start_date') }} <br/>
-            {{ html()->input('date','start_date')}}<br/>
-            {{ html()->label('Seleccione fecha de egreso','end_date') }} <br/>
-            {{ html()->input('date','end_date')}} <br/>
-            {{html()->submit('enviar')->class('p-2 m-2 bg-blue-600')}}
+        <form method="POST" class="grid grid-flow-row-dense grid-cols-2 gap-5 grid-rows-7 ">
+            {{ html()->token() }}
+            <!--fullname and age inputs-->
+            <div class="grid content-stretch">
+                {{ html()->text('names')->attributes(['placeholder'=>'Ingrese los nombres','wire:model'=>'names'])->class('') }} 
+                <div>@error('names') {{ $message }} @enderror</div>
+            </div>
+            <div class="grid">
+                {{ html()->text('surnames')->class('')->attributes(['placeholder'=>'Ingrese los apellidos','wire:model'=>'surnames']) }}
+                <div>@error('surnames') {{ $message }} @enderror</div>
+            </div>
+
+            <div class="grid">
+                {{ html()->text('age')->attributes(['pattern'=>'[0-9]{2}','placeholder'=>'Ingrese la edad','wire:model'=>'age'])->class('') }} 
+                <div>@error('age') {{ $message }} @enderror</div>
+            </div>
+
+            <!--gender select-->
+            <div class="grid">
+                {{ html()->label('Seleccione el genero','gender_id')->class('') }}
+                {{ html()->select('gender_id',[1=>'Masculino',2=>'Femenino'])->attributes(['wire:model'=>'gender_id'])->class('')}} 
+                <div>@error('gender_id') {{ $message }} @enderror</div>
+            </div>
+
+            <!--id.doc and crdential inputs-->
+            <div class="grid">
+                {{ html()->text('credential')->attributes(['placeholder'=>'Ingrese la credencial','pattern'=> '[0-9]{9}','wire:model'=>'credential'])->class('')}}
+                <div>@error('credential') {{ $message }} @enderror</div>
+            </div>
+
+            <div class="grid">
+                {{ html()->text('identity_document')->attributes(['placeholder'=>'Ingrese la cedula','wire:model'=>'identity_document'])->class('')}}
+                <div>@error('identity_document') {{ $message }} @enderror</div>
+            </div>
+
+            <!--promo. range, charge,status and dependency selects-->
+            <div class="grid">
+                {{ html()->label('Seleccione la promo','promo_id')->class('') }}
+                {{ html()->select('promo_id',$data['promos'],0)->class('')->attributes(['wire:model'=>'promo_id']) }}
+                <div>@error('promo_id') {{ $message }} @enderror</div>
+            </div>
+
+            <div class="grid">
+                {{ html()->label('Seleccione el rango','rank_id')->class('') }} <br/>
+                {{ html()->select('rank_id',$data['ranks'])->class('')->attributes(['wire:model'=>'rank_id']) }} <br/>
+                <div>@error('rank_id') {{ $message }} @enderror</div>
+            </div>
+
+            <div class="grid">
+                {{ html()->label('Seleccione el cargo','charge_id')->class('') }} <br/>
+                {{ html()->select('charge_id',$data['charges'],0)->class('')->attributes(['wire:model'=>'charge_id']) }} <br/>
+                <div>@error('charge_id') {{ $message }} @enderror</div>
+            </div>
+
+            <div class="grid">
+                {{ html()->label('Seleccione la dependencia','dependency_id')->class('') }} <br/>
+                {{ html()->select('dependency_id',$data['dependencies'],0)->class('')->attributes(['wire:model'=>'dependency_id','default'=>1]) }} <br/>
+                <div>@error('dependency_id') {{ $message }} @enderror</div>
+            </div>
+
+            <div class="grid">
+                {{ html()->label('Seleccione el estado','status_id')->class('') }} <br/>
+                {{ html()->select('status_id',$data['statuses'],0)->class('')->attributes(['wire:model'=>'status_id']) }} <br/>
+                <div>@error('status_id') {{ $message }} @enderror</div>
+            </div>
+
+            <!--start and end date input-->
+            <div class="grid">
+                {{ html()->label('Seleccione fecha de ingreso','start_date')->class('') }} <br/>
+                {{ html()->input('date','start_date')->class('')->attributes(['wire:model'=>'start_date'])}}<br/>
+                <div>@error('start_date') {{ $message }} @enderror</div>
+            </div>  
+            {{-- {{ html()->label('Seleccione fecha de egreso','end_date')->class('m-2') }} <br/>
+            {{ html()->input('date','end_date')->class('m-2')->attributes(['wire:model'=>'end_date']) }} <br/>
+            <div>@error('end_date') {{ $message }} @enderror</div> --}}
+            
+            <input class="col-span-2" type="submit" value="Añadir" wire:click.prevent='save'>
         </form>
         
     </div>
